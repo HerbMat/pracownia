3bbb63b9fba3492736824b70bb000d2ce5249e69
bzielinski91 <bzielinski91@wp.pl>
2017-01-10 11:43:23 +0100

    Kolejna próba kalendarza

--- www/js/app.js
+++ www/js/app.js
@@ -5,11 +5,11 @@
 // the 2nd parameter is an array of 'requires'
 
 
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','ngRoute','ui.router'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if (cordova.platformId === "ios" && window.cordova && window.cordova.plugins.Keyboard) {
+    /*if (cordova.platformId === "ios" && window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -18,11 +18,43 @@ angular.module('starter', ['ionic'])
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
-    }
+    }*/
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
+})
+
+
+
+
+
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+	
+	.state('calendar', {
+		
+		url: '/calendar',
+		templateUrl: 'templates/calendar.html',
+		controller: 'CalendarCtrl'
+	})
+
+    .state('login', {
+        
+        url: '/login',
+        templateUrl: 'templates/login.html',
+        controller: 'LoginCtrl'
+    })
+    
+    // if none of the above states are matched, use this as the fallback
+    
+
 })
 
 
